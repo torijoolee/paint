767a191f262211cd54f5069ab12ebb5e11955d55
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-04-09 00:02:26 +0900

    JS:erase,paint

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const erase = document.querySelector(".eraser");
+const paint = document.querySelector(".paint");
 canvas.width = 610;
 canvas.height = 460;
 
 let isPsinting = false;
+let isFillMode = false;
 
+//drawing
 function onMoveMouse(event) {
   if (isPsinting) {
     console.log(event.offsetX, event.offsetY);
@@ -19,7 +23,25 @@ function mouseDown() {
 }
 function mouseUp() {
   isPsinting = false;
+  ctx.beginPath();
 }
+//erase
+function eraserBtn() {
+  ctx.strokeStyle = "white";
+}
+//paint
+function fillMode() {
+  isFillMode = true;
+}
+function fillPage() {
+  if (isFillMode) {
+    ctx.fillRect(0, 0, 610, 460);
+  }
+}
+
+canvas.addEventListener("click", fillPage);
+paint.addEventListener("click", fillMode);
+erase.addEventListener("click", eraserBtn);
 canvas.addEventListener("mousemove", onMoveMouse);
 canvas.addEventListener("mousedown", mouseDown);
 document.addEventListener("mouseup", mouseUp);
