348788376f8d47e1c89c0656deeb428d8dc5caf5
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-04-09 13:40:03 +0900

    HTML:data-color

--- js/main.js
+++ js/main.js
@@ -2,45 +2,58 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const erase = document.querySelector(".eraser");
 const paint = document.querySelector(".paint");
+const deletePage = document.querySelector(".del");
 canvas.width = 610;
 canvas.height = 460;
 
-let isPsinting = false;
+let isPainting = false;
 let isFillMode = false;
 
 //drawing
 function onMoveMouse(event) {
-  if (isPsinting) {
-    console.log(event.offsetX, event.offsetY);
+  if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
+    return;
   }
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
 function mouseDown() {
-  isPsinting = true;
+  isPainting = true;
 }
 function mouseUp() {
-  isPsinting = false;
+  isPainting = false;
   ctx.beginPath();
 }
-//erase
-function eraserBtn() {
-  ctx.strokeStyle = "white";
-}
+
 //paint
-function fillMode() {
-  isFillMode = true;
+function handleFillMode() {
+  if (isFillMode) {
+    isFillMode = false;
+  } else {
+    isFillMode = true;
+  }
 }
 function fillPage() {
   if (isFillMode) {
     ctx.fillRect(0, 0, 610, 460);
   }
 }
+//erase
+function eraserBtn() {
+  ctx.strokeStyle = "white";
+  isFillMode = false;
+}
+//deletePage
+function deleteAllPage() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, 610, 460);
+}
 
+deletePage.addEventListener("click", deleteAllPage);
 canvas.addEventListener("click", fillPage);
-paint.addEventListener("click", fillMode);
+paint.addEventListener("click", handleFillMode);
 erase.addEventListener("click", eraserBtn);
 canvas.addEventListener("mousemove", onMoveMouse);
 canvas.addEventListener("mousedown", mouseDown);
