8c17872bafb82e0dcca228a8590ab5aa15e506f2
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-04-09 14:10:44 +0900

    lineWidth

--- js/main.js
+++ js/main.js
@@ -3,11 +3,23 @@ const ctx = canvas.getContext("2d");
 const erase = document.querySelector(".eraser");
 const paint = document.querySelector(".paint");
 const deletePage = document.querySelector(".del");
+const colorChips = document.querySelectorAll(".color-chip");
+const inputRange = document.querySelector(".rangeInput");
 canvas.width = 610;
 canvas.height = 460;
 
 let isPainting = false;
 let isFillMode = false;
+ctx.lineCap = "round";
+
+colorChips.forEach((color) => {
+  color.addEventListener("click", function (event) {
+    console.log(event.target.dataset.color);
+    const colorPick = event.target.dataset.color;
+    ctx.fillStyle = colorPick;
+    ctx.strokeStyle = colorPick;
+  });
+});
 
 //drawing
 function onMoveMouse(event) {
@@ -51,6 +63,11 @@ function deleteAllPage() {
   ctx.fillRect(0, 0, 610, 460);
 }
 
+function checkLineWidth(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+inputRange.addEventListener("change", checkLineWidth);
 deletePage.addEventListener("click", deleteAllPage);
 canvas.addEventListener("click", fillPage);
 paint.addEventListener("click", handleFillMode);
